a7c73afcc232bc832e4e2aee8025a202430afc1e
rockleona <34214497+rockleona@users.noreply.github.com>
2024-12-12 18:20:35 +0800

    feat(user): initialize user create

---
+++ lib/db/index.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+import process from 'process';
+
+class MongooseClient {
+    private uri: string;
+
+    constructor() {
+        this.uri = process.env.MONGO_URI || '';
+    }
+
+    public async connect(): Promise<void> {
+        try {
+            await mongoose.connect(this.uri);
+            console.log('Successfully connected to MongoDB');
+        } catch (error) {
+            console.error('Error connecting to MongoDB:', error);
+            throw error;
+        }
+    }
+
+    public async disconnect(): Promise<void> {
+        try {
+            await mongoose.disconnect();
+            console.log('Successfully disconnected from MongoDB');
+        } catch (error) {
+            console.error('Error disconnecting from MongoDB:', error);
+            throw error;
+        }
+    }
+}
+
+export default MongooseClient;
---
+++ lib/db/user.ts
@@ -0,0 +1,75 @@
+import { Schema, model, HydratedDocument, Model } from "mongoose";
+import MongooseClient from ".";
+
+interface UserInterface {
+    username: string;
+    password: string;
+    role: string;
+    reviews: {
+        review: string;
+        reviewer: string;
+        rating: string;
+        feedback: string | null;
+    }[];
+};
+
+
+class UserDBClient extends MongooseClient {
+    private schema: Schema;
+    private model: Model<UserInterface>;
+
+    constructor() {
+        super()
+        this.checkCollection()
+    }
+
+    private async checkCollection() {
+        this.schema = new Schema<UserInterface>({
+            username: { type: String, required: true },
+            password: { type: String, required: true },
+            role: { type: String, required: true },
+            reviews: [{
+                review: { type: String, required: true },
+                reviewer: { type: String, required: true },
+                rating: { type: String, required: true },
+                feedback: { type: String, required: false },
+            }],
+        });
+
+        this.model = model<UserInterface>('User') || model<UserInterface>('User', this.schema);
+    }
+
+    // private model = model<UserInterface>('User', this.schema);
+
+    public async create(user: UserInterface): Promise<UserInterface> {
+        try {
+            const newUser: HydratedDocument<UserInterface> = new this.model(user);
+            console.log(newUser);
+            return await newUser.save();
+        } catch (error) {
+            console.error('Error creating user:', error);
+            throw error;
+        }
+    }
+
+    public async read(username: string): Promise<UserInterface | null> {
+        try {
+            return await this.model.findOne().where('username').equals(username).exec();
+        } catch (error) {
+            console.error('Error reading user:', error);
+            throw error;
+        }
+    }
+
+    public async update(username: string, user: UserInterface): Promise<UserInterface | null> {
+        try {
+            return await this.model.findOneAndUpdate().where('username').equals(username).update(user).exec();
+        } catch (error) {
+            console.error('Error updating user:', error);
+            throw error;
+        }
+    }
+
+}
+
+export default UserDBClient;
