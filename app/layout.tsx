88adc84c8467633bc09f9c5a299d392578da4ec2
rockleona <34214497+rockleona@users.noreply.github.com>
2024-12-17 13:53:52 +0800

    fix(middleware): path rewrite best practice applied

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,6 @@ import type { Metadata } from "next";
 import NextLink from "next/link";
 import localFont from "next/font/local";
 import "./globals.css";
-import { cookies } from "next/headers";
-import { decrypt } from "@/app/lib/session"
-import { Button } from "@headlessui/react";
-import { destroySession } from "@/app/lib/session";
-import { redirect } from "next/navigation";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,19 +19,11 @@ export const metadata: Metadata = {
   description: "An example of a Next.js app",
 };
 
-const logoutAction = async () => {
-  destroySession();
-  redirect('/login');
-}
-
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const cookie_store = await cookies()
-  const session_cookie = cookie_store.get('session')
-  const session = await decrypt(session_cookie?.value)
 
   return (
     <html lang="en">
@@ -50,12 +37,6 @@ export default async function RootLayout({
             <NextLink href="/performance">Performance Review</NextLink>
             <NextLink href="/employees">Employees</NextLink>
           </div>
-          {session &&
-            <div className="flex space-x-5 items-center">
-              <span>{session.username as string}</span>
-              <Button onClick={}>Logout</Button>
-            </div>
-          }
         </div>
         {children}
       </body>
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,15 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
- 
+
 export function middleware(request: NextRequest) {
   const session_cookie = request.cookies.get("session")
-  if(!session_cookie) {
-    return NextResponse.redirect('/login')
+  if (!session_cookie) {
+    return NextResponse.redirect(new URL('/login', request.url))
   }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/'],
 }
