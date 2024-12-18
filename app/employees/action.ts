5b557471c23458fbde1c009e72c8ad0abc8f2b6c
rockleona <34214497+rockleona@users.noreply.github.com>
2024-12-18 20:46:52 +0800

    refactor: project file structure moved

---
+++ app/employees/action.ts
@@ -0,0 +1,69 @@
+import { SignupFormSchema, FormState } from '@/app/lib/definitions'
+import bcrypt from 'bcryptjs'
+
+export async function create(state: FormState, formData: FormData) {
+    // Validate form fields
+    const validatedFields = SignupFormSchema.safeParse({
+        username: formData.get('username'),
+        password: formData.get('password'),
+    })
+
+    // If any form fields are invalid, return early
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+        }
+    }
+
+    const { username, password } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    // Create a new user
+
+    const response = await fetch('/api/signup', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: username,
+            password: hashedPassword,
+            role: formData.get('role'),
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    let data;
+    try {
+        data = await response.json();
+    } catch (error) {
+        throw new Error('Failed to parse response JSON');
+    }
+
+    if (!response.ok) {
+        return {
+            errors: {
+                username: ['An error occurred while creating the user. Please try again.'],
+            },
+        }
+    }
+
+    if (data.status === 200) {
+        return {
+            message: 'User created successfully'
+        }
+    } else {
+        return {
+            errors: {
+                username: ['An error occurred while creating the user. Please try again.'],
+            },
+        }
+    }
+};
+
+export async function update(state: FormState, formData: FormData){
+    // TODO: Implement update user
+}
+
+export async function remove(state: FormState, formData: FormData){
+    // TODO: Implement remove user
+}
